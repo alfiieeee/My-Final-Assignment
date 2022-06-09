dfe466486098d5ed25773efb2699b3ab0292366c
Nazirul Fikri <106502768+alfiieeee@users.noreply.github.com>
2022-06-09 10:12:56 +0800

    Update coding.py

--- coding.py
+++ coding.py
@@ -38,7 +38,7 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-st.write(iris.target_names)
+st.write(iris.target_names ['setosa', 'versicolor', 'virginica'])
 
 st.subheader('Prediction')
 st.write(iris.target_names['setosa', 'versicolor', 'virginica'])
