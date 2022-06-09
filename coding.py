717524386d1b4cb3bdf8d05c7703997157e19f02
Nazirul Fikri <106502768+alfiieeee@users.noreply.github.com>
2022-06-09 10:21:25 +0800

    Update coding.py

--- coding.py
+++ coding.py
@@ -27,7 +27,7 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-
+iris = pd.read_csv('https://raw.githubusercontent.com/alfiieeee/My-Final-Assignment/main/IRIS.csv')
 X = iris.drop ('species', axis = 1)
 Y = iris['species']
 
@@ -38,11 +38,11 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-st.write(iris.target_names)
+iris.target_names = ['setosa','versicolor','virginica']
 
 st.subheader('Prediction')
-st.write(iris.target_names['Iris-setosa', 'Iris-versicolor', 'Iris-virginica'])
-#st.write(prediction)
+st.write(iris.target_names[prediction])
+st.write(prediction)
 
 st.subheader('Prediction Probability')
 st.write(prediction_proba)
